bbc8a6fa4a7b0d764856e0c5c9c0dd7faf27d35a
Ricardo Polanski <ricardo.polanski@ericsson.com>
2024-02-20 20:41:09 -0300

    getting elements and typing into them / getting alert box text

--- test/specs/firstTest.js
+++ test/specs/firstTest.js
@@ -1,7 +1,13 @@
 describe('Ecomerce Application', () => {
     it('Login Fail Page', async () => {
         await browser.url("https://rahulshettyacademy.com/loginpagePractise");
+        await expect(browser).toHaveTitle('LoginPage Practise | Rahul Shetty Academy')
         console.log(await browser.getTitle());
-
+        await $("#username").setValue("testing")
+        await $("#password").setValue("password")
+        await $("#signInBtn").click();
+        await browser.pause(3000)
+        const text = (await($(".alert-danger").getText()))
+        await console.log(text)
     })
 })
