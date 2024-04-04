8d720321178c517247e31fda5095370246db62a1
Ricardo Polanski <rpolanski@live.com>
2024-04-04 19:40:29 -0300

    swith tablePages

---
+++ test/specs/windowsFrames.js
@@ -0,0 +1,16 @@
+import { expect as expectChai } from 'chai'
+
+describe('Windows and Frames', async () => {
+    it('Parent and Child Windows Switch', async () => {
+        await browser.url('https://rahulshettyacademy.com/loginpagePractise/')
+        await $('.blinkingText').click()
+        const window = await browser.getWindowHandles()
+        await browser.switchToWindow(window[1])
+        const h1Text = await $("h1").getText()
+        expectChai(h1Text).to.be.eq('DOCUMENTS REQUEST')
+        await browser.closeWindow()
+        await browser.switchToWindow(window[0])
+        await browser.getTitle()
+
+    })
+})
