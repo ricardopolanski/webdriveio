a1a4f25518c1539ef9a438f7d35e1738bf02d4c0
Ricardo Polanski <rpolanski@live.com>
2024-04-04 19:19:40 -0300

    webtables

--- test/specs/webTables.js
+++ test/specs/webTables.js
@@ -1,7 +1,7 @@
 import { expect as expectChai } from 'chai'
 
 describe('Web Tables Tests', () => {
-    it('', async () => {
+    it('Sort Table Vales', async () => {
         await browser.url('https://rahulshettyacademy.com/seleniumPractise/#/offers')
         await $('tr th:nth-child(1)').click()
         const list = await $$('tr td:nth-child(1)')
@@ -9,5 +9,15 @@ describe('Web Tables Tests', () => {
         const veggiesNames = await list.map(veggies => veggies.getText())
         const sortedVeggies = veggiesNames.sort()
         expectChai(veggiesNames).to.eq(sortedVeggies)
+    })
+
+    it('Search Field', async () => {
+        await browser.url('https://rahulshettyacademy.com/seleniumPractise/#/offers')
+        await $('#search-field').setValue('Tomato')
+        const veggieLocator = await $$('tr td:nth-child(1)')
+        
+        expect(veggieLocator).toBeElementsArrayOfSize( { eq:1 } )
+        const veggie = await veggieLocator[0].getText()
+        expectChai(veggie).to.be.eq('Tomato')
     })
 })
